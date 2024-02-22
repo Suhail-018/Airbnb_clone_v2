525adb630bd7d1ab54066a9a2c756a8c4d9f0863
suhail-09 <you@example.com>
2024-02-21 18:58:21 -0800

    web framework

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""using flask for web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+    """display hello world"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
