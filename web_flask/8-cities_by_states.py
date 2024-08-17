947959d97b769be6d240a19f8c29e475036da93a
Suhail-018 <107703767+Suhail-018@users.noreply.github.com>
2024-08-17 15:34:36 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """display a HTML page: (inside the tag BODY)"""
-    # sort states by name from a to z
+    
     slist = sorted(storage.all(
         State).values(), key=lambda x: x.name)
-    # sort cities by name from a to z
+    
    for s in slist:
        s.cities.sort(key=lambda x: x.name)
     return render_template("8-cities_by_states.html", sorted_states_list=slist)
